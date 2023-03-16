bacc850292bc14a4436170567bc03bac0ec68139
tf2-doc <rbueno.100.b@gmail.com>
2023-03-15 23:23:20 -0400

    a good amount of olivol

---
+++ scripts/draw.js
@@ -0,0 +1,4 @@
+const canvas = document.getElementsByTagName('canvas');
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+const ctx = canvas.getContext("2d");
